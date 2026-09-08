5ee414707ac145be0d33137f58f3577821f7eca9
Thu Nguyen <thuthien2001@gmail.com>
2026-09-08 20:57:46 +0700

    fix (backend): use product slug for cart

--- backend/RipperdocShop.Api/Modules/Carts/Commands/UpsertCartItemCommand.cs
+++ backend/RipperdocShop.Api/Modules/Carts/Commands/UpsertCartItemCommand.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using RipperdocShop.Api.Data;
 using RipperdocShop.Api.Models.Entities;
 using RipperdocShop.Api.Modules.Carts.Errors;
@@ -7,10 +8,16 @@ namespace RipperdocShop.Api.Modules.Carts.Commands;
 
 public class UpsertCartItemCommand(ApplicationDbContext dbContext)
 {
-    public async Task<CartItem> ExecuteAsync(Guid productId, Guid userId, int quantity)
+    public async Task<CartItem> ExecuteAsync(string productSlug, Guid userId, int quantity)
     {
+        var product = await dbContext.Products
+            .Include(p => p.Category)
+            .Include(p => p.Brand)
+            .FirstOrDefaultAsync(p => p.Slug == productSlug) ?? throw new ProductNotFoundException(productSlug);
+        if (product is not { DeletedAt: null }) throw new ProductNotFoundException(productSlug);
+
         var existingItem = dbContext.CartItems
-            .FirstOrDefault(ci => ci.ProductId == productId && ci.UserId == userId);
+            .FirstOrDefault(ci => ci.ProductId == product.Id && ci.UserId == userId);
 
         if (existingItem is not null)
         {
@@ -18,12 +25,10 @@ public class UpsertCartItemCommand(ApplicationDbContext dbContext)
             await dbContext.SaveChangesAsync();
             return existingItem;
         }
-        
-        var product = await dbContext.Products.FindAsync(productId) ?? throw new ProductNotFoundException(productId);  
-        var newItem = new CartItem(product, userId, quantity);       
+
+        var newItem = new CartItem(product, userId, quantity);
         dbContext.CartItems.Add(newItem);
         await dbContext.SaveChangesAsync();
         return newItem;
-
     }
 }
--- backend/RipperdocShop.Api/Modules/Carts/Errors/ProductNotFoundException.cs
+++ backend/RipperdocShop.Api/Modules/Carts/Errors/ProductNotFoundException.cs
@@ -2,7 +2,7 @@ using RipperdocShop.Api.Infrastructure.Errors;
 
 namespace RipperdocShop.Api.Modules.Carts.Errors;
 
-public class ProductNotFoundException(Guid productId)
-    : DomainException($"Product {productId} is not found", StatusCodes.Status400BadRequest)
+public class ProductNotFoundException(string product)
+    : DomainException($"Product {product} is not found", StatusCodes.Status400BadRequest)
 {
 }
